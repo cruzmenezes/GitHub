189de24cb7dc716141a7235f837ad166b051e7ea
Edson Victor da cruz Menezes <91231848+cruzmenezes@users.noreply.github.com>
2023-07-09 14:15:08 +0000

    objeto chave

--- adaJS/objeto.js
+++ adaJS/objeto.js
@@ -4,7 +4,24 @@ let pessoa = {
     name: 'edson',
     age: '42',
     city: 'SP',
+    country: 'Brasil'
 }
 
 console.log(pessoa);
 console.log(pessoa.name);
+
+// ADCIONANDO UM ELEMENTO NO OBJETO
+pessoa.altura = 1.67
+console.log(pessoa);
+
+// REMOVENDO ELEMENTO NO OBJETO
+
+delete pessoa.altura
+
+console.log(pessoa);
+
+console.clear()
+
+for(let chave in pessoa){
+    console.log(chave)
+}
