265487f4be68c46d35f0bee76935d7c56b23abf6
Edson Victor da cruz Menezes <91231848+cruzmenezes@users.noreply.github.com>
2023-07-14 21:54:00 +0000

    lista

--- adaJS/listaArray.js
+++ adaJS/listaArray.js
@@ -1,5 +1,36 @@
-let minhaLista = ['carro', 42, 'casa']
+let minhaLista = [10, 42, 150, 35, 50, 3, 2, 1]
 
 for(let elemento of minhaLista){
-    console.log(elemento)
+
+    console.log(typeof(elemento))
 }
+
+console.clear();
+
+
+// soma lista 
+
+let acumulador = 0;
+
+for(let numero of minhaLista){
+    acumulador += numero;
+}
+
+console.log(`A soma da lista é: ${acumulador}`);
+
+
+// Qual o menor numero da lista
+
+let menorNumero;
+
+for (let numero of minhaLista){
+    if(menorNumero === undefined){
+        menorNumero = numero;
+    }else {
+        if(numero < menorNumero){
+            menorNumero = numero
+        }
+    }
+}
+
+console.log(`O menor numero da lisra é: ${menorNumero}`);
